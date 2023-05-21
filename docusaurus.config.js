bc5d43319151b8d7d2a70b0daeb46876fd2f5142
Ryder Belserion <no-reply@ryderbelserion.com>
2023-05-21 00:16:56 -0400

    It's not defaulting to dark

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,6 +54,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false
+      },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
