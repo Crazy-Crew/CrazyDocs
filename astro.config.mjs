b7290d82402527d845eb76125a529f16cb56d928
Ryder Belserion <contact@ryderbelserion.com>
2026-04-26 01:29:09 -0400

    Add vuejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,72 +4,72 @@ import starlight from '@astrojs/starlight';
 
 import topics from 'starlight-sidebar-topics'
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://docs.crazycrew.us',
+    site: 'https://docs.crazycrew.us',
 
-	image: {
-		responsiveStyles: true,
-		layout: "constrained",
-		domains: ["cdn.ryderbelserion.com"],
-	},
+    image: {
+        responsiveStyles: true,
+        layout: "constrained",
+        domains: ["cdn.ryderbelserion.com"],
+    },
 
-	integrations: [
-		starlight({
-			title: 'CrazyCrew Docs',
+    integrations: [starlight({
+        title: 'CrazyCrew Docs',
 
-			favicon: 'https://assets.crazycrew.us/crazycrew/branding/crazycrew_logo.32.svg',
+        favicon: 'https://assets.crazycrew.us/crazycrew/branding/crazycrew_logo.32.svg',
 
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/Crazy-Crew'},
-				{ icon: 'discord', label: 'Discord', href: 'https://discord.gg/badbones-s-live-chat-182615261403283459'},
-				{ icon: 'patreon', label: 'Kofi', href: 'https://ko-fi.com/ryderbelserion' }
-			],
+        social: [
+            { icon: 'github', label: 'GitHub', href: 'https://github.com/Crazy-Crew'},
+            { icon: 'discord', label: 'Discord', href: 'https://discord.gg/badbones-s-live-chat-182615261403283459'},
+            { icon: 'patreon', label: 'Kofi', href: 'https://ko-fi.com/ryderbelserion' }
+        ],
 
-			lastUpdated: true,
-			editLink: {
-				baseUrl: 'https://github.com/Crazy-Crew/CrazyDocs/edit/main/',
-			},
+        lastUpdated: true,
+        editLink: {
+            baseUrl: 'https://github.com/Crazy-Crew/CrazyDocs/edit/main/',
+        },
 
-			credits: true,
+        credits: true,
 
-			components: {
-				// Override the default `Sidebar` component with a custom one.
-				Sidebar: './src/components/Sidebar.astro',
-			},
+        components: {
+            // Override the default `Sidebar` component with a custom one.
+            Sidebar: './src/components/Sidebar.astro',
+        },
 
-			plugins: [
-				topics([
-					{
-						id: "crazycrew",
-						label: "Home",
-						link: "/getting_started/overview/",
-						icon: "information",
-						items: [
-							"getting_started/overview"
-						],
-					},
-					{
-						id: "crazycrates",
-						label: "CrazyCrates",
-						link: "/mods/crazycrates/",
-						icon: "open-book",
-						items: [
-							{
-								label: "Administration",
-								items: [
-									"crazycrates/faq",
-									"crazycrates/support"
-								],
-							},
-							{
-								label: "Developers",
-								items: [],
-							}
-						],
-					}
-				])
-			]
-		})
-	]
+        plugins: [
+            topics([
+                {
+                    id: "crazycrew",
+                    label: "Home",
+                    link: "/getting_started/overview/",
+                    icon: "information",
+                    items: [
+                        "getting_started/overview"
+                    ],
+                },
+                {
+                    id: "crazycrates",
+                    label: "CrazyCrates",
+                    link: "/mods/crazycrates/",
+                    icon: "open-book",
+                    items: [
+                        {
+                            label: "Administration",
+                            items: [
+                                "crazycrates/faq",
+                                "crazycrates/support"
+                            ],
+                        },
+                        {
+                            label: "Developers",
+                            items: [],
+                        }
+                    ],
+                }
+            ])
+        ]
+		}), vue()]
 });
